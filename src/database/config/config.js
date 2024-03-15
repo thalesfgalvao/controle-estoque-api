cefcaf5fff651693ef9965d4301b822e8c504242
Thales Marques <thalesf.galvao@gmail.com>
2024-03-15 15:11:34 -0300

    Migration de usuarios, ajuste de arquivo de config e sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: __dirname + '/../../../.env' });
+require('dotenv').config({ path: __dirname + '/./../../../.env' });
 
 module.exports = {
   development: {
-    username: 'root',
-    password: '973128',
-    database: 'controle_estoque',
+    username: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     host: '172.17.0.2',
     dialect: 'mysql',
     logging: false,
